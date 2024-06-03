785264eefbb1f0fc3af54925ce4095df467648c9
kovalj1337 <vanaplayers@gmail.com>
2024-06-03 20:07:39 +0400

    lol

--- index.php
+++ index.php
@@ -19,44 +19,58 @@
                 oninput="this.nextElementSibling.value = this.value">
             <input type="text" name="sugar" value="1" oninput="this.previousElementSibiling.value = this.value">
             <button type="submit">submit</button>
-            <input type="radio" name="chai" value="easy" id="easy">
+            <input type="radio" name="time" value="2" id="easy">
             <label for="easy">Легкий</label>
-            <input type="radio" name="chai" value="medium" id="medium">
+            <input type="radio" name="time" value="5" id="medium">
             <label for="medium">Середній</label>
-            <input type="radio" name="chai" value="chefir" id="chefir">
-            <label for="medium">Сильний</label>
+            <input type="radio" name="time" value="10" id="chefir">
+            <label for="chefir">Сильний</label>
         </div>
     </form>
     <?php
     if ($_POST) {
         $voda = $_POST["voda"];
         $sugar = $_POST["sugar"];
-        if(isset($_POST['chai'])){
-            $chai = $_POST["chai"];  
-        } else{
-            $chai = "0";
+        $time = $_POST['time'];
+        echo('<p>' .$voda . "мл замовлено </p>");
+        echo('<p>' .$sugar . "ложок замовлено </p>");
+        echo('<p>'. $time . "хвилин заварити </p>");
+        if($voda <= 250){
+            $onecuponesugar = 1;
+        }else{
+            $onecuponesugar = 0;
         }
         $cup = 250;
-        $timeForEasy = 2;
-        $timeForMedium = 5;
-        $timeForChefir = 10;
         $minValueVoda = 50;
         if($voda < $cup || $voda > $cup ||$voda == $cup){
             for($voda; $voda >= 0; $voda -= $cup){
                 if($voda <= $cup || $voda >= $cup || $voda == $cup){
+                    echo("<p>$voda</p>");
                     for($i = 50; $i <= $cup && $i <= $voda; $i += $minValueVoda){
                         $result = ($minValueVoda * $sugar / $voda) * 5; 
                         echo("Налито \n" . $i ."\n води");
                     }
-                    if($voda <= 250){
-                        $sugar = $sugar * $minValueVoda / $cup;
-                        echo("Насипано " . $sugar . "ложок цукру");
-                    }else{
-                        $result = ($minValueVoda * $sugar / $voda) * 5; 
-                        $sugar -= $result;
-                        echo("Насипано " . $result . "ложок цукру");
-                    }
                     echo("Чашка повна");
+                    if($onecuponesugar == 0){
+                        if($voda <= 250){
+                            $dolinaodne = $voda * 5 / 250;
+                            $sugar = ($sugar * $minValueVoda / $cup) * $dolinaodne;
+                            echo("Насипано " . $sugar . "ложок цукру");
+                            $timeforone = $voda * 5 / 250;
+                            $time = ($time * $minValueVoda / $cup) * $timeforone;
+                            echo("чай робився " . $time . "хвилин");
+                        }else{
+                            $result = round(($minValueVoda * $sugar / $voda) * 5, 2); //round - функція для округлення чисел
+                            $sugar -= $result;
+                            echo("Насипано " . $result . "ложок цукру");
+                            $timefortea = round(($minValueVoda * $time / $voda) * 5, 2);
+                            $time -= $timefortea;
+                            echo("чай робився " . $timefortea . "хвилин");
+                        }
+                    }else{
+                        echo("Насипано " . $sugar . "ложок цукру");
+                        echo("чай робився " . $time . "хвилин");
+                    }
                 }
             }
         }
